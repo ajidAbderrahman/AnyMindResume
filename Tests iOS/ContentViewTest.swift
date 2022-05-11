88ae38e95472bed81d8677839df302df0a5aa8ee
Abderrahman Ajid <abderrahman.ajid@gmail.com>
2022-05-11 18:40:06 +0100

    test hello world

--- Tests iOS/ContentViewTest.swift
+++ Tests iOS/ContentViewTest.swift
@@ -6,8 +6,17 @@
 //
 
 import XCTest
+import ViewInspector
+@testable import AnyMindResume
+
+extension ContentView: Inspectable {}
 
 class ContentViewTest: XCTestCase {
-
-
+    
+    func testStringValue() throws {
+        let sut = ContentView()
+        let value = try sut.inspect().text().string()
+        XCTAssertEqual(value, "Hello, world!")
+    }
+    
 }
