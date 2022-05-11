bc06716e064dc8f40abc15c1a3510fd68a2cf3c8
Abderrahman Ajid <abderrahman.ajid@gmail.com>
2022-05-11 17:50:03 +0100

    Initial Commit

---
+++ Shared/AnyMindResumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnyMindResumeApp.swift
+//  Shared
+//
+//  Created by Abderrahman Ajid on 11/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct AnyMindResumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
