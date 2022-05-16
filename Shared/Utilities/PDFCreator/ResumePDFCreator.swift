4ffe13124d1bb576e136f7cc56e2a4543bb07e39
Abderrahman Ajid <abderrahman.ajid@gmail.com>
2022-05-16 21:42:01 +0100

    - add works to resumePDFCreator - add comments to resumePDFCreator

--- Shared/Utilities/PDFCreator/ResumePDFCreator.swift
+++ Shared/Utilities/PDFCreator/ResumePDFCreator.swift
@@ -22,6 +22,52 @@ final class ResumePDFCreator {
     private let textFontSize: CGFloat = 18
     private let titleFontSize: CGFloat = 25
     
+    private var addNewPage: (() -> Void)?
+    
+    // return the pdf drawing as a Data object
+    private var data: Data? {
+        
+        guard let renderer = pdfCreator.renderer else {
+            return nil
+        }
+        
+        let data = renderer.pdfData { context in
+            context.beginPage()
+            addPersonalInfo(resume.personalInfo)
+            addSkills(resume.skills)
+            addWorks(resume.works)
+        }
+        return data
+    }
+    
+    var resumePDF: PDFDocument? {
+        pdfCreator.pdfDoc(data)
+    }
+    
+    // provide size and position for each element
+    //also calculate total Hegiht for all elements
+    private func rect(for type: ElementType) -> CGRect {
+        var rectangle = CGRect.zero
+        switch type {
+        case .image: rectangle = CGRect(x: 10, y: totalHeight, width: 100, height: 100)
+        case .title: rectangle = CGRect(x: 10, y: totalHeight, width: pageRect.width - 20, height: 35)
+        case .text: rectangle = CGRect(x: 10, y: totalHeight, width: pageRect.width - 20, height: 25)
+        case .paragraph: rectangle = CGRect(x: 10, y: totalHeight, width: pageRect.width - 20, height: 80)
+        }
+        totalHeight += rectangle.height
+        return rectangle
+    }
+    
+    // PDF supported elements
+    private enum ElementType {
+        case image
+        case title
+        case text
+        case paragraph
+    }
+    
+    // MARK: Resume helpers method
+    
     private func addPersonalInfo(_ info: PersonalInfo) {
         pdfCreator.addImage(info.image, rect: rect(for: .image))
         pdfCreator.addText(info.fullName, rect: rect(for: .title), size: titleFontSize)
@@ -40,42 +86,13 @@ final class ResumePDFCreator {
         }
     }
     
-    private func pdfData() -> Data? {
-        
-        guard let renderer = pdfCreator.renderer else {
-            return nil
+    private func addWorks(_ works: [Work]) {
+        pdfCreator.addText("Works", rect: rect(for: .title), size: titleFontSize)
+        works.forEach { work in
+            pdfCreator.addText("Company name: \(work.name)", rect: rect(for: .text), size: textFontSize)
+            pdfCreator.addText("Start Date: \(work.startDate)", rect: rect(for: .text), size: textFontSize)
+            pdfCreator.addText("End Date: \(work.endDate)", rect: rect(for: .text), size: textFontSize)
         }
-        
-        let data = renderer.pdfData { context in
-            context.beginPage()
-            addPersonalInfo(resume.personalInfo)
-            addSkills(resume.skills)
-            
-        }
-        return data
-    }
-    
-    var resumePDF: PDFDocument? {
-        pdfCreator.pdfDoc(pdfData())
-    }
-    
-    private enum ElementType {
-        case image
-        case title
-        case text
-        case paragraph
-    }
-    
-    private func rect(for type: ElementType) -> CGRect {
-        var rectangle = CGRect.zero
-        switch type {
-        case .image: rectangle = CGRect(x: 10, y: totalHeight, width: 100, height: 100)
-        case .title: rectangle = CGRect(x: 10, y: totalHeight, width: pageRect.width - 20, height: 35)
-        case .text: rectangle = CGRect(x: 10, y: totalHeight, width: pageRect.width - 20, height: 25)
-        case .paragraph: rectangle = CGRect(x: 10, y: totalHeight, width: pageRect.width - 20, height: 80)
-        }
-        totalHeight += rectangle.height
-        return rectangle
     }
     
 }
